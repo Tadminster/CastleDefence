75f3ae1001ed4705ec1ce9152f61eecb9cdbc2b7
Tadminster <91bottlenine@naver.com>
2023-06-09 03:31:36 +0900

    Mini World Sprite https://merchant-shade.itch.io/16x16-mini-world-sprites

---
+++ Contents/Images/MiniWorldSprites/Buildings/Red/testMap.lua
@@ -0,0 +1,54 @@
+----------------------------------------------------------
+-- 
+----------------------------------------------------------
+
+project = {
+    assets = {
+        [0] = {
+            asset_id = "WinterTrees",
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Nature\\WinterTrees.png"
+        },
+        [1] = {
+            asset_id = "PineTrees",
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Nature\\PineTrees.png"
+        },
+        [2] = {
+            asset_id = "WinterDeadTrees",
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Nature\\WinterDeadTrees.png"
+        },
+        [3] = {
+            asset_id = "Cactus",
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Nature\\Cactus.png"
+        },
+        [4] = {
+            asset_id = "CoconutTrees",
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Nature\\CoconutTrees.png"
+        },
+        [5] = {
+            asset_id = "Trees",
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Nature\\Trees.png"
+        },
+        [6] = {
+            asset_id = "Rocks",
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Nature\\Rocks.png"
+        },
+        [7] = {
+            asset_id = "Grass",
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Ground\\Grass.png"
+        },
+        [8] = {
+            asset_id = "RedKeep",
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Buildings\\Red\\RedKeep.png"
+        }
+    },
+    maps = {
+        [0] = {
+            file_path = "C:\\Users\\null\\Downloads\\MiniWorldSprites\\MiniWorldSprites\\Buildings\\Red\\testMap.map",
+        },
+    },
+    canvas = {
+        canvas_width = 1600,
+        canvas_height = 1600,
+        tile_size = 16
+    }
+}

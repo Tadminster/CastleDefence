1c457532325a1f178aa4db55726f578265befc8c
Tadminster <91bottlenine@naver.com>
2023-06-12 03:47:18 +0900

    새 이미지 path 추가

--- Game2/FilePath.cpp
+++ Game2/FilePath.cpp
@@ -8,13 +8,21 @@
 FilePath::FilePath()
 {
     this->path.insert(std::make_pair("Grass", ".\\MiniWorldSprites\\Ground\\Grass.png"));
+    this->path.insert(std::make_pair("Grass2", ".\\MiniWorldSprites\\Ground\\Grass2.png"));
+    this->path.insert(std::make_pair("Cliff", ".\\MiniWorldSprites\\Ground\\Cliff.png"));
+    this->path.insert(std::make_pair("TexturedGrass", ".\\MiniWorldSprites\\Ground\\TexturedGrass.png"));
+
+    this->path.insert(std::make_pair("Trees", ".\\MiniWorldSprites\\Nature\\Trees.png"));
+    this->path.insert(std::make_pair("Wheatfield", ".\\MiniWorldSprites\\Nature\\Wheatfield.png"));
+
     this->path.insert(std::make_pair("RedKeep", ".\\MiniWorldSprites\\Buildings\\Red\\RedKeep.png"));
     this->path.insert(std::make_pair("Houses", ".\\MiniWorldSprites\\Buildings\\Wood\\Houses.png"));
     this->path.insert(std::make_pair("Tower", ".\\MiniWorldSprites\\Buildings\\Wood\\Tower.png"));
+    this->path.insert(std::make_pair("Tower2", ".\\MiniWorldSprites\\Buildings\\Wood\\Tower2.png"));
     this->path.insert(std::make_pair("Taverns", ".\\MiniWorldSprites\\Buildings\\Wood\\Taverns.png"));
-    this->path.insert(std::make_pair("Trees", ".\\MiniWorldSprites\\Nature\\Trees.png"));
     this->path.insert(std::make_pair("Barracks", ".\\MiniWorldSprites\\Buildings\\Wood\\Barracks.png"));
-    this->path.insert(std::make_pair("Wheatfield", ".\\MiniWorldSprites\\Nature\\Wheatfield.png"));
+    this->path.insert(std::make_pair("LimeTower", ".\\MiniWorldSprites\\Buildings\\Lime\\LimeTower.png"));
+
     this->path.insert(std::make_pair("Bridge", ".\\MiniWorldSprites\\Miscellaneous\\Bridge.png"));
     this->path.insert(std::make_pair("QuestBoard", ".\\MiniWorldSprites\\Miscellaneous\\QuestBoard.png"));
     this->path.insert(std::make_pair("StreetSigns", ".\\MiniWorldSprites\\Miscellaneous\\StreetSigns.png"));

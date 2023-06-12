38c4dab86232a3372f09382eeec2cf1e0299ab2d
Tadminster <91bottlenine@naver.com>
2023-06-13 02:13:34 +0900

    몬스터매니저 생성

---
+++ Game2/MonsterManager.cpp
@@ -0,0 +1,34 @@
+﻿#include "stdafx.h"
+#include "Monster.h"
+#include "MonsterManager.h"
+
+
+
+void MonsterManager::Init()
+{
+}
+
+void MonsterManager::Release()
+{
+}
+
+void MonsterManager::Update()
+{
+	for (auto& enemy : this->enemy)
+		enemy->Update();
+}
+
+void MonsterManager::LateUpdate()
+{
+}
+
+void MonsterManager::Render()
+{
+	for (auto& enemy : this->enemy)
+		enemy->Render();
+}
+
+void MonsterManager::AddEnemy(class Monster* monster)
+{
+	this->enemy.emplace_back(monster);
+}
---
+++ Game2/MonsterManager.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#define MONSTER		MonsterManager::GetInstance()
+
+class MonsterManager : public Singleton<MonsterManager>
+{
+private:
+	vector<class Monster*> enemy;
+
+public:
+	void	Init();
+	void	Release();
+	void	Update();
+	void	LateUpdate();
+	void	Render();
+
+	void	AddEnemy(Monster* monster);
+
+	int		getEnemyCount() { return enemy.size(); }
+};
+

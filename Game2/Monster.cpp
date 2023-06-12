497f723b2667d272790916098c05175100d40c10
Tadminster <91bottlenine@naver.com>
2023-06-13 02:14:04 +0900

    몬스터 조상 클래스 생성

--- Game2/Monster.cpp
+++ Game2/Monster.cpp
@@ -1,1 +1,27 @@
 ﻿#include "Monster.h"
+
+Monster::Monster() :
+	collider(new ObRect()), 
+	skin_run(nullptr),
+	dir(MonsterDir::D)
+	{}
+
+void Monster::Init()
+{
+}
+
+void Monster::Update()
+{
+	this->collider->Update();
+
+	if (this->skin_run)
+		this->skin_run->Update();
+}
+
+void Monster::Render()
+{
+	this->collider->Render();
+
+	if (this->skin_run)
+		this->skin_run->Render();
+}
--- Game2/Monster.h
+++ Game2/Monster.h
@@ -1,5 +1,24 @@
-﻿#pragma once
+﻿#include "stdafx.h"
+#pragma once
+enum class MonsterDir
+{
+	U, D, L, R
+};
+
 class Monster
 {
+protected:
+	ObRect* collider;
+	ObImage* skin_run;
+	MonsterDir dir;
+
+public:
+	//Monster() {};
+	Monster();
+	virtual ~Monster() {};
+
+	virtual void Init();
+	virtual void Update();
+	virtual void Render();
 };
 

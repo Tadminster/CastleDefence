f6300b867022fb001d938e6c3f3fa18f244cb74b
Tadminster <91bottlenine@naver.com>
2023-06-13 02:14:18 +0900

    슬라임 클래스 생성

---
+++ Game2/Slime.cpp
@@ -0,0 +1,42 @@
+﻿#include "Slime.h"
+
+Slime::Slime()
+{
+	this->collider = new ObRect();
+	this->skin_run = new ObImage(L"Slime.png");
+	this->skin_run->SetParentRT(*this->collider);
+}
+
+Slime::~Slime()
+{
+	delete collider;
+	delete skin_run;
+
+	TEXTURE->DeleteTexture(L"Slime.png");
+}
+
+void Slime::Init()
+{
+	this->collider->SetWorldPos(Vector2(0, 0));
+	this->collider->isFilled = false;
+
+	this->collider->scale = Vector2(100, 100);
+	this->skin_run->scale = Vector2(100, 100);
+}
+
+void Slime::Update()
+{
+	Monster::Update();
+	//this->collider->Update();
+	//this->skin_idle->Update();
+	//this->skin_run->Update();
+}
+
+void Slime::Render()
+{
+
+	Monster::Render();
+	//this->collider->Render();
+	//this->skin_idle->Render();
+	//this->skin_run->Render();
+}
---
+++ Game2/Slime.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Monster.h"
+
+class Slime : public Monster
+{
+public:
+	Slime();
+	~Slime();
+
+	void Init();
+	void Update();
+	void Render();
+};
+

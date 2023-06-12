a8a797982883c160a49a365400017a5c0236f1ba
Tadminster <91bottlenine@naver.com>
2023-06-13 02:14:31 +0900

    게임 매니저 생성

---
+++ Game2/GameManager.cpp
@@ -0,0 +1,32 @@
+﻿#include "stdafx.h"
+#include "Player.h"
+#include "GameManager.h"
+
+GameManager::~GameManager()
+{
+	player->~Player();
+	delete player;
+}
+
+void GameManager::Init()
+{
+	player = new Player();
+}
+
+void GameManager::Release()
+{
+}
+
+void GameManager::Update()
+{
+}
+
+void GameManager::LateUpdate()
+{
+}
+
+void GameManager::Render()
+{
+}
+
+
---
+++ Game2/GameManager.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#define GAME_MANAGER		GameManager::GetInstance()
+
+class GameManager : public Singleton<GameManager>
+{
+private:
+
+public:
+	class Player* player;
+	~GameManager();
+
+	void	Init();
+	void	Release();
+	void	Update();
+	void	LateUpdate();
+	void	Render();
+
+
+};
+

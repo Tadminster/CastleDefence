a7f1f6a5e2c82785665f4f481e5e3fc61aa4fa19
Tadminster <91bottlenine@naver.com>
2023-06-13 03:23:56 +0900

    게임매니저 stdafx에 등록

--- Game2/stdafx.h
+++ Game2/stdafx.h
@@ -1,3 +1,10 @@
-#pragma once
+﻿#pragma once
 #include "../2DFrameWork/framework.h"
 #pragma comment (lib,"../Build/2DFrameWork/2DFrameWork.lib")
+
+
+#include "GameManager.h"
+#include "Player.h"
+
+//Singleton Macro
+#define GM		GameManager::GetInstance()

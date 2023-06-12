69f71994fa2691666f1fc664081bd09455d5e68e
Tadminster <91bottlenine@naver.com>
2023-06-12 23:00:23 +0900

    타일맵 이름변경, 위치변경

---
+++ Contents/Save/TileMap_2.lua
@@ -0,0 +1,114 @@
+----------------------------------------------------------
+-- 
+----------------------------------------------------------
+
+project = {
+    assets = {
+        [0] = {
+            asset_id = "Grass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass.png"
+        },
+        [1] = {
+            asset_id = "TexturedGrass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\TexturedGrass.png"
+        },
+        [2] = {
+            asset_id = "Grass2",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass2.png"
+        },
+        [3] = {
+            asset_id = "Grass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass.png"
+        },
+        [4] = {
+            asset_id = "TexturedGrass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\TexturedGrass.png"
+        },
+        [5] = {
+            asset_id = "Grass2",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass2.png"
+        },
+        [6] = {
+            asset_id = "Grass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass.png"
+        },
+        [7] = {
+            asset_id = "TexturedGrass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\TexturedGrass.png"
+        },
+        [8] = {
+            asset_id = "Grass2",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass2.png"
+        },
+        [9] = {
+            asset_id = "Grass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass.png"
+        },
+        [10] = {
+            asset_id = "TexturedGrass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\TexturedGrass.png"
+        },
+        [11] = {
+            asset_id = "Grass2",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass2.png"
+        },
+        [12] = {
+            asset_id = "Grass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass.png"
+        },
+        [13] = {
+            asset_id = "TexturedGrass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\TexturedGrass.png"
+        },
+        [14] = {
+            asset_id = "Grass2",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass2.png"
+        },
+        [15] = {
+            asset_id = "Grass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass.png"
+        },
+        [16] = {
+            asset_id = "TexturedGrass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\TexturedGrass.png"
+        },
+        [17] = {
+            asset_id = "Grass2",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass2.png"
+        },
+        [18] = {
+            asset_id = "Grass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass.png"
+        },
+        [19] = {
+            asset_id = "TexturedGrass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\TexturedGrass.png"
+        },
+        [20] = {
+            asset_id = "Grass2",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass2.png"
+        },
+        [21] = {
+            asset_id = "Grass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass.png"
+        },
+        [22] = {
+            asset_id = "TexturedGrass",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\TexturedGrass.png"
+        },
+        [23] = {
+            asset_id = "Grass2",
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Contents\\Images\\MiniWorldSprites\\Ground\\Grass2.png"
+        }
+    },
+    maps = {
+        [0] = {
+            file_path = "C:\\Users\\null\\source\\repos\\Tadminster\\CastleDefence\\Save\\TileMap_4.map",
+        },
+    },
+    canvas = {
+        canvas_width = 1280,
+        canvas_height = 1280,
+        tile_size = 32
+    }
+}

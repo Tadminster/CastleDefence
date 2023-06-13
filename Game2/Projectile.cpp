f18c4d62979f0a5d9c1fdf3752251bf97730b886
tadminster <91bottlenine@naver.com>
2023-06-13 16:59:02 +0900

    발사체 클래스 생성

---
+++ Game2/Projectile.cpp
@@ -0,0 +1,33 @@
+﻿#include "stdafx.h"
+#include "Projectile.h"
+
+Projectile::Projectile
+(
+    Vector2 spawnPos,
+    Vector2 dir,
+    float speed,
+    float range,
+    float damage
+) :
+    skin(nullptr),
+    speed(speed),
+    range(range),
+    traveledDistance(0.f),
+    damage(damage)
+{
+    this->scale.x = 10;
+    this->scale.y = 10;
+    this->isFilled = true;
+}
+
+void Projectile::Update()
+{
+    // V2_탄속만 적용
+    Vector2 velocity = (this->dir * this->speed);
+
+    this->MoveWorldPos(velocity * DELTA);
+    ObRect::Update();
+
+    // 탄이 이동한 거리 계산
+    traveledDistance += std::sqrt(std::pow(speed * DELTA, 2) + std::pow(speed * DELTA, 2));
+}
---
+++ Game2/Projectile.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+class Projectile : public ObRect
+{
+public:
+    ObImage* skin;
+
+    Vector2         dir;                    // 방향
+    float           speed;                  // 속도
+    int             damage;                 // 공격력
+    float           range;                  // 사정거리
+    float           traveledDistance;       // 이동거리
+
+public:
+    Projectile(Vector2 spawnPos, Vector2 dir, float speed, float range, float damage);
+    void Update() override;
+
+    /* 탄이 이동한 거리가 range를 넘었는지 확인 */
+    bool hasTraveledTooFar() const { return traveledDistance >= range; }
+};
+

362d628b3ef3767423f87f4b9af33d31ab387e3e
tadminster <91bottlenine@naver.com>
2023-06-13 16:59:14 +0900

    무기 클래스 생성

---
+++ Game2/Weapon.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+class Weapon
+{
+protected:
+	string		name;
+	float		attackSpeed;
+	float		damage;
+	float		projectileSpeed;
+	float		range;
+	float		critical;
+
+	float		timeSinceLastTime;
+	float		lastShotTime;
+
+public:
+	virtual bool Attack() = 0;
+};
+

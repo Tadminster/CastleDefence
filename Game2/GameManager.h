5c67f8d3b2fc8c2eac09db8dce4b84a33a0ea319
TADMINSTER <91bottlenine@naver.com>
2023-06-15 16:09:39 +0900

    kill, level, exp 변수 생

--- Game2/GameManager.h
+++ Game2/GameManager.h
@@ -6,6 +6,11 @@ class GameManager : public Singleton<GameManager>
 public:
 	class Player* player;
 	class MonsterManager* monster;
+
+	int kill;
+	int level;
+	int exp;
+	int nextExp[];
 	
 public:
 	~GameManager();

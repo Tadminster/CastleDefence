e5c642b344be49ad78935a939e740c37272116cb
tadminster <91bottlenine@naver.com>
2023-06-15 21:45:21 +0900

    hud 클래스 생성

---
+++ Game2/HUD.cpp
@@ -0,0 +1,65 @@
+﻿#include "stdafx.h"
+#include "HUD.h"
+
+HUD::HUD()
+{
+	exp_box = new ObImage(L"ui_box_exp.png");
+	exp_gauge = new ObImage(L"ui_gauge_exp.png");
+	hp_box = new ObImage(L"ui_box_hp.png");
+	hp_gauge = new ObImage(L"ui_gauge_hp.png");
+
+	exp_gauge->SetParentRT(*exp_box);
+	hp_gauge->SetParentRT(*hp_box);
+
+	exp_box->space = SPACE::SCREEN;
+	exp_gauge->space = SPACE::SCREEN;
+	hp_box->space = SPACE::SCREEN;
+	hp_gauge->space = SPACE::SCREEN;
+}
+
+HUD::~HUD()
+{
+	delete exp_box;
+	delete exp_gauge;
+	delete hp_box;
+	delete hp_gauge;
+}
+
+void HUD::Init()
+{
+	
+	exp_box->pivot = OFFSET_L;
+	exp_box->SetWorldPos(Vector2(-app.GetHalfWidth(), app.GetHalfHeight()-9));
+	exp_box->scale.x = app.GetWidth();
+	exp_box->scale.y = exp_box->imageSize.y * 2;
+
+	exp_gauge->pivot = OFFSET_L;
+	exp_gauge->scale.x = 0;
+	exp_gauge->scale.y = exp_box->imageSize.y * 2;
+}
+
+void HUD::Release()
+{
+}
+
+void HUD::Update()
+{
+	exp_gauge->scale.x = (GM->player->exp / GM->nextExp[GM->player->level]) * app.GetWidth();
+
+	exp_box->Update();
+	exp_gauge->Update();
+	hp_box->Update();
+	hp_gauge->Update();
+}
+
+void HUD::LateUpdate()
+{
+}
+
+void HUD::Render()
+{
+	exp_box->Render();
+	exp_gauge->Render();
+	hp_box->Render();
+	hp_gauge->Render();
+}
---
+++ Game2/HUD.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+class HUD
+{
+private:
+	class ObImage* exp_box;
+	class ObImage* exp_gauge;
+
+	class ObImage* hp_box;
+	class ObImage* hp_gauge;
+
+public:
+	HUD();
+	~HUD();
+	void Init();
+	void Release(); //해제
+	void Update();
+	void LateUpdate();//갱신
+	void Render();
+};
+
